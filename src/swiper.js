986adc614be8e4124c18957d12ef64be1759eb93
Kostia31 <kostyapodnebesnuy@gmail.com>
2023-03-03 12:18:00 +0200

    ds

---
+++ src/swiper.js
@@ -0,0 +1,33 @@
+import Swiper from 'swiper/bundle';
+
+// import styles bundle
+import 'swiper/css/bundle';
+const swiper = new Swiper('.slider-img', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+  },
+
+  preloadImages: true,
+  slidesPerView: 1.5,
+  simulateTouch: true,
+  touchRatio: 3,
+  grabCursor: true,
+  spaceBetween: 20,
+  initialSlide: 0,
+
+  breakpoints: {
+    768: {
+      slidesPerView: 1.5,
+    },
+
+    1440: {
+      slidesPerView: 1.5,
+    },
+  },
+});
