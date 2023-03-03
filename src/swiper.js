726bb2aec1a6e5ac01598466021ae55646b0cc5c
Kostia31 <kostyapodnebesnuy@gmail.com>
2023-03-03 23:12:05 +0200

    Update swiper.js

--- src/swiper.js
+++ src/swiper.js
@@ -1,10 +1,13 @@
 import Swiper from 'swiper/bundle';
 
+// import styles bundle
+import 'swiper/css/bundle';
+
 const swiper = new Swiper('.slider-img', {
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
-  },
+  },    
 
   keyboard: {
     enabled: true,
